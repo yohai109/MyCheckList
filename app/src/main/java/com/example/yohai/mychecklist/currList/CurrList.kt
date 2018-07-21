512d69bf64f3e7b52cb68998474379642b0e39d4
Yohai Knaani <yohai109@gmail.com>
2018-07-21 16:25:47 +0300

    created the currListFragment

---
+++ app/src/main/java/com/example/yohai/mychecklist/currList/CurrList.kt
@@ -0,0 +1,31 @@
+package com.example.yohai.mychecklist.currList
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.yohai.mychecklist.R
+
+class CurrList : Fragment() {
+
+    companion object {
+        fun newInstance() = CurrList()
+    }
+
+    private lateinit var viewModel: CurrListViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.curr_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(CurrListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/yohai/mychecklist/currList/CurrListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.yohai.mychecklist.currList
+
+import androidx.lifecycle.ViewModel;
+
+class CurrListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
