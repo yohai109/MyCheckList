7f68b95dfb61a7a09be95ab6d98309922338b4c6
Yohai Knaani <yohai109@gmail.com>
2018-07-21 16:47:49 +0300

    add the currListFragment to the main activity

--- app/src/main/java/com/example/yohai/mychecklist/MainActivity.kt
+++ app/src/main/java/com/example/yohai/mychecklist/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.yohai.mychecklist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.yohai.mychecklist.currList.CurrList
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        addFragment("firstFragment",R.id.fragment_holder){CurrList.newInstance()}
+    }
+}
+
+fun AppCompatActivity.addFragment(tag: String, layoutId: Int, allowStateLoss: Boolean = false,
+                                  newInstance: () -> Fragment) {
+    val fragment = supportFragmentManager.findFragmentByTag(tag) ?: newInstance()
+    val transaction = supportFragmentManager.beginTransaction()
+            .replace(layoutId, fragment, tag)
+    if (allowStateLoss) {
+        transaction.commitAllowingStateLoss()
+    } else {
+        transaction.commit()
     }
 }
